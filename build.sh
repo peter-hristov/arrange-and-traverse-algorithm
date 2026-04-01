61c69ec64311b6e571c8a86b57956b48b5b01ef9
Peter Hristov <peter.g.hristov@gmail.com>
2026-04-01 18:18:18 +0200

    checkpont

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# Optionally read how many cores to use
+CORES=${1:-$(nproc)}
+
 # Set up working folder
 projectFolder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "The current folder is $projectFolder"
 
+# Set up where are going to clones and compile all dependencies
 mkdir libraries
 
-# VTK 
+## VTK 
 echo "Building VTK"
 
 cd $projectFolder/libraries
@@ -16,12 +20,12 @@ cd vtk
 mkdir build install
 cd build
 
-cmake -DCMAKE_INSTALL_PREFIX="$projectFolder/libraries/vtk/install" -DCMAKE_BUILD_TYPE="Release" ..
-make -j 4
+cmake -DCMAKE_INSTALL_PREFIX="$projectFolder/libraries/install/vtk" -DCMAKE_BUILD_TYPE="Release" ..
+make -j$CORES
 make install
     
 
-## CGAL
+### CGAL
 echo "CGAL"
 
 cd $projectFolder/libraries
@@ -31,11 +35,11 @@ cd ./cgal
 mkdir build install
 cd build
 
-cmake -DCMAKE_INSTALL_PREFIX="$projectFolder/libraries/cgal/install" -DCMAKE_BUILD_TYPE="Release" ..
-make -j 4
+cmake -DCMAKE_INSTALL_PREFIX="$projectFolder/libraries/install/cgal" -DCMAKE_BUILD_TYPE="Release" ..
+make -j$CORES
 make install
 
-## TTK
+### TTK
 echo "TTK"
 
 cd $projectFolder/libraries
@@ -45,9 +49,18 @@ cd ./ttk
 mkdir build install
 cd build
 
-cmake -DCMAKE_INSTALL_PREFIX="$projectFolder/libraries/ttk/install" -DCMAKE_BUILD_TYPE="Release" -DTTK_BUILD_PARAVIEW_PLUGINS="Off" -DCMAKE_PREFIX_PATH="$projectFolder/libraries/VTK-9.4.1/install" ..
-make -j 4
+cmake -DCMAKE_INSTALL_PREFIX="$projectFolder/libraries/install/ttk" -DCMAKE_BUILD_TYPE="Release" -DTTK_BUILD_PARAVIEW_PLUGINS="Off" -DCMAKE_PREFIX_PATH="$projectFolder/libraries/install/vtk" ..
+make -j$CORES
 make install
+
+
+### RS Explorer
+cd $projectFolder/build
+cmake -DCMAKE_PREFIX_PATH="$projectFolder/libraries/install/cgal;$projectFolder/libraries/install/vtk;$projectFolder/libraries/install/ttk" -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DCMAKE_BUILD_TYPE=Release ..
+make
+
+cd $projectFolder
+
 
 
 
