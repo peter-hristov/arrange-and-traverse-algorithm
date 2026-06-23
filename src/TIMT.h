47e82404b785a9399a3369d3c7d74b0124df74d9
Peter Hristov <peter.g.hristov@gmail.com>
2026-06-23 12:12:31 +0100

    Beta version with train outside the image.

---
+++ src/TIMT.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "./TetMesh.h"
+
+namespace timt
+{
+    void generateTopologyGraph(TetMesh &tetMesh, const std::array<double, 2> &p);
+
+    void writeTopologyGraphToVTP(
+            const std::vector<std::array<float, 3>> &domainCoordinates,
+            const std::vector<std::pair<int, int>> &edges,
+            const std::vector<double> &vertexDistances,
+            const std::string &filename);
+
+};
